ab70a8db8a87e28bbcf00c5a09bcc96519a0c62a
baizhoufun <chengzhe.zhou@exchange.com>
2020-04-08 09:52:44 +0800

    .gitignore is now working

---
+++ build/CMakeFiles/QSpline.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QSpline.dir/src/quinticSpline.cpp.o"
+  "libQSpline.pdb"
+  "libQSpline.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QSpline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
